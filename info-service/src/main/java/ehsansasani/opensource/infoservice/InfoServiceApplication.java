2d0093525696712fa4818f1c7aa9172e9b2c3ad7
Ehsan Sasanian <ehsan.sasanian@gmail.com>
2020-04-20 11:53:58 +0430

    first commit

---
+++ info-service/src/main/java/ehsansasani/opensource/infoservice/InfoServiceApplication.java
@@ -0,0 +1,13 @@
+package ehsansasani.opensource.infoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfoServiceApplication.class, args);
+	}
+
+}
---
+++ info-service/src/test/java/ehsansasani/opensource/infoservice/InfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ehsansasani.opensource.infoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
